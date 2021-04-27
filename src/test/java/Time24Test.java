00a3f511108d2788e85a703b8abd1341bbd9692e
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 13:45:43 -0600

    Update Time24Test.java

--- src/test/java/Time24Test.java
+++ src/test/java/Time24Test.java
@@ -6,7 +6,11 @@ public class Time24Test
     @Test
     public void NormalAfternoonTime_Valid()
     {
-        Time24 time = Time24.toTime24(3, 0, AmPm.pm);
+        try
+        {
+            Time24 time = Time24.toTime24(3, 0, AmPm.pm);
+        } catch (InvalidTimeException e) {}
+        
         String timeString = time.toString();
         assertEquals("15:00", timeString);
     }
@@ -14,7 +18,11 @@ public class Time24Test
     @Test
     public void TwelveOClockAfternoonTime_Valid()
     {
-        Time24 time = Time24.toTime24(12, 27, AmPm.pm);
+        try
+        {
+            Time24 time = Time24.toTime24(12, 27, AmPm.pm);
+        } catch (InvalidTimeException e) {}
+        
         String timeString = time.toString();
         assertEquals("12:27", timeString);
     }
@@ -22,7 +30,11 @@ public class Time24Test
     @Test
     public void NormalMorning_Valid()
     {
-        Time24 time = Time24.toTime24(6, 30, AmPm.am);
+        try
+        {
+            Time24 time = Time24.toTime24(6, 30, AmPm.am);
+        } catch (InvalidTimeException e) {}
+        
         String timeString = time.toString();
         assertEquals(" 6:30", timeString);
     }
@@ -30,7 +42,11 @@ public class Time24Test
     @Test
     public void TwelveOClockMorning_Valid()
     {
-        Time24 time = Time24.toTime24(12, 45, AmPm.am);
+        try
+        {
+            Time24 time = Time24.toTime24(12, 45, AmPm.am);
+        } catch (InvalidTimeException e) {}
+        
         String timeString = time.toString();
         assertEquals(" 0:45", timeString);
     }
