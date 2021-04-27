e0c2a4c47db016a2c6a5d182fd21a1c23568b41f
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 15:21:40 -0600

    Create Time12Test.java

---
+++ src/test/java/Time12Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test; //JUnit5
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Time12Test
+{
+    @Test
+    public void HourGreater_MinuteGreater_PMvsAM_Positive()
+    {
+        try
+        {
+            Time12 t1 = new Time12(12, 45, AmPm.pm);
+            Time12 t2 = new Time12(8, 36, AmPm.am);
+            assertEquals(249, Time12.subtract(t1, t2));
+        }
+    }
+}
