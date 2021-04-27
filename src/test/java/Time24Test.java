b33c60b48e8cbd990429d998687ef17ea13a0238
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 12:58:35 -0600

    Create Time24Test.java

---
+++ src/test/java/Time24Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test; //JUnit5
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Time24Test
+{
+    @Test
+    public void NormalAfternoonDate_Valid()
+    {
+        int hours = 3;
+        int minutes = 0;
+        AmPm ampm = AmPm.pm;
+        
+        Time24 time = Time24.toTime24(hours, minutes, ampm);
+        String timeString = time.toString();
+        assertEquals("15:00", timeString);
+    }
+}
