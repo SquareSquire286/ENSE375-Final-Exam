bc255f476fc07c0e4165fb71dd1d03e10cd33912
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 13:21:31 -0600

    Update Time24Test.java

--- src/test/java/Time24Test.java
+++ src/test/java/Time24Test.java
@@ -4,14 +4,70 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Time24Test
 {
     @Test
-    public void NormalAfternoonDate_Valid()
+    public void NormalAfternoonTime_Valid()
     {
-        int hours = 3;
-        int minutes = 00;
-        AmPm ampm = AmPm.pm;
-        
-        Time24 time = Time24.toTime24(hours, minutes, ampm);
+        Time24 time = Time24.toTime24(3, 0, AmPm.pm);
         String timeString = time.toString();
         assertEquals("15:00", timeString);
+    }
+    
+    @Test
+    public void TwelveOClockAfternoonTime_Valid()
+    {
+        Time24 time = Time24.toTime24(12, 27, AmPm.pm);
+        String timeString = time.toString();
+        assertEquals("12:27", timeString);
+    }
+    
+    @Test
+    public void NormalMorning_Valid()
+    {
+        Time24 time = Time24.toTime24(6, 30, AmPm.am);
+        String timeString = time.toString();
+        assertEquals(" 6:30", timeString);
+    }
+    
+    @Test
+    public void TwelveOClockMorning_Valid()
+    {
+        Time24 time = Time24.toTime24(12, 45, AmPm.am);
+        String timeString = time.toString();
+        assertEquals(" 0:45", timeString);
+    }
+    
+    @Test
+    public void AfternoonTime_MinuteException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(3, 65, AmPm.pm);});
+    }
+    
+    @Test
+    public void AfternoonTime_HourException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(25, 30, AmPm.pm);});
+    }
+    
+    @Test
+    public void TwelveOClockAfternoon_MinuteException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(12, -15, AmPm.pm);});
+    }
+    
+    @Test
+    public void TwelveOClockMorning_MinuteException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(12, 90, AmPm.am);});
+    }
+    
+    @Test
+    public void NormalMorningTime_HourException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(-4, 30, AmPm.am);});
+    }
+    
+    @Test
+    public void NormalMorningTime_MinuteException_Invalid()
+    {
+        assertThrows(InvalidTimeException.class, () -> { Time24 time = Time24.toTime24(8, -20, AmPm.am);});
     }
 }
