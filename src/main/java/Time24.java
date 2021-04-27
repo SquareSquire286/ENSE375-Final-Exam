bd83c4d7cb47d5b638b2c8cbbc2af796eae1af98
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 13:38:46 -0600

    Update Time24.java

--- src/main/java/Time24.java
+++ src/main/java/Time24.java
@@ -41,7 +41,7 @@ public class Time24
 	 * @see 	https://www.freecodecamp.org/news/mathematics-converting-am-pm-to-24-hour-clock/
 	 * @note	(12 am and 12 pm are special cases)
 	 */
-	public static Time24 toTime24(int hours, int minutes, AmPm am_pm) throws InvalidTimeException
+	public static Time24 toTime24(int hours, int minutes, AmPm am_pm)
 	{
 		Time24 time = null;
 
