66e7f78c3b870447096cb1c03d5d57265c06f4a9
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-27 15:22:54 -0600

    Update Time12Test.java

--- src/test/java/Time12Test.java
+++ src/test/java/Time12Test.java
@@ -11,6 +11,6 @@ public class Time12Test
             Time12 t1 = new Time12(12, 45, AmPm.pm);
             Time12 t2 = new Time12(8, 36, AmPm.am);
             assertEquals(249, Time12.subtract(t1, t2));
-        }
+        } catch (Exception e) {}
     }
 }
